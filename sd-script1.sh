800a0bae9f878b71bd5daf51fa177486da9855c4
Gunasekaran <703625094@SanfordHealth.org>
2024-05-28 17:23:27 -0500

    host name changes

--- sd-script1.sh
+++ sd-script1.sh
@@ -4,10 +4,10 @@ DB_HOST="${DB_HOST}"
 DB_PORT="${DB_PORT}"
 DB_NAME="${DB_NAME}"
 DB_USER="${DB_USER}"
-DB_PASSWORD="${PGPASSWORD}"
+DB_PASSWORD="postgres"
 # PGPASSWORD="${DB_PASSWORD}"  # Use the environment variable for password
 SQL_FILE="./release1.sql"
-echo  $DB_PASSWORD
+echo  "Selva": $DB_PASSWORD
 # Check if the database is ready
 pg_isready -h "$DB_HOST" -p "$DB_PORT" -U "$DB_USER" -d "$DB_NAME"
 psql -h "$DB_HOST" -p "$DB_PORT" -U "$DB_USER" -d "$DB_NAME" -f "$SQL_FILE"
