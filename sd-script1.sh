b97fa97f3c27fb57ff43736e789199e0e2092156
Gunasekaran <703625094@SanfordHealth.org>
2024-05-28 09:30:15 -0500

    host name changes

--- sd-script1.sh
+++ sd-script1.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Database connection parameters
 # DB_HOST="selva-geneartdb.c7gozccxmuzm.us-east-1.rds.amazonaws.com"
-DB_HOST="172.16.2.154"
+DB_HOST= ${DB_HOST:-default_value}
 DB_PORT="5432"
 DB_NAME="geneart"
 DB_USER="postgres"
@@ -9,6 +9,8 @@ DB_PASSWORD=$DB_PASSWORD
 export PGPASSWORD="$DB_PASSWORD"
 # Check network connectivity
 echo "Testing connectivity to the database host..."
+echo "database host: '$DB_HOST'"
+psql -h $DB_HOST -p "$DB_PORT" -U "$DB_USER" -d "$DB_NAME" -c "SELECT 1;"
 # Connect to the database
-pg_isready -h "$DB_HOST" -p "$DB_PORT" -U "$DB_USER" -d "$DB_NAME"
-psql -h "$DB_HOST" -p "$DB_PORT" -U "$DB_USER" -d "$DB_NAME" -c "SELECT 1;"
+# pg_isready -h "$DB_HOST" -p "$DB_PORT" -U "$DB_USER" -d "$DB_NAME"
+# psql -h "$DB_HOST" -p "$DB_PORT" -U "$DB_USER" -d "$DB_NAME" -c "SELECT 1;"
