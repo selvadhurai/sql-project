198321feb485116cea70861b9ba18531807c8187
Gunasekaran <703625094@SanfordHealth.org>
2024-05-28 09:03:51 -0500

    host name changes

--- sd-script1.sh
+++ sd-script1.sh
@@ -9,14 +9,6 @@ DB_PASSWORD=$DB_PASSWORD
 export PGPASSWORD="$DB_PASSWORD"
 # Check network connectivity
 echo "Testing connectivity to the database host..."
-# Trim any potential whitespace from the hostname
-DB_HOST=$(echo "$DB_HOST" | tr -d '[:space:]')
-# Find the IP address of the host
-echo "Finding the IP address of the database host..."
-nslookup "$DB_HOST" || dig +short "$DB_HOST"
-# Check network connectivity
-echo "Testing connectivity to the database host..."
-ping -c 4 "$DB_HOST"
 # Connect to the database
 pg_isready -h "$DB_HOST" -p "$DB_PORT" -U "$DB_USER" -d "$DB_NAME"
 psql -h "$DB_HOST" -p "$DB_PORT" -U "$DB_USER" -d "$DB_NAME" -c "SELECT 1;"
