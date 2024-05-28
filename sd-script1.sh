bb6a50f8c92fd2dcf6d0e0cc31bc025297e266f8
Gunasekaran <703625094@SanfordHealth.org>
2024-05-28 16:41:15 -0500

    host name changes

--- sd-script1.sh
+++ sd-script1.sh
@@ -1,28 +1,11 @@
 #!/bin/bash
-set -e
-
 # Ensure the environment variables are available
 DB_HOST="${DB_HOST}"
 DB_PORT="${DB_PORT}"
 DB_NAME="${DB_NAME}"
 DB_USER="${DB_USER}"
 PGPASSWORD="${DB_PASSWORD}"  # Use the environment variable for password
-
 SQL_FILE="./release1.sql"
-
 # Check if the database is ready
 pg_isready -h "$DB_HOST" -p "$DB_PORT" -U "$DB_USER" -d "$DB_NAME"
-if [ $? -eq 0 ]; then
-    echo "Database is ready. Executing SQL file..."
-    # Execute the SQL file
-    psql -h "$DB_HOST" -p "$DB_PORT" -U "$DB_USER" -d "$DB_NAME" -f "$SQL_FILE"
-    if [ $? -eq 0 ]; then
-        echo "SQL file executed successfully"
-    else
-        echo "Error executing SQL file"
-        exit 1
-    fi
-else
-    echo "Database is not ready or authentication failed. Transaction terminated."
-    exit 1
-fi
+psql -h "$DB_HOST" -p "$DB_PORT" -U "$DB_USER" -d "$DB_NAME" -f "$SQL_FILE"
