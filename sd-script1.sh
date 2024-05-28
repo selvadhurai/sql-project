08e90987d84cccec17b530e5f8a5024a1c9160f5
Gunasekaran <703625094@SanfordHealth.org>
2024-05-28 14:30:40 -0500

    host name changes

--- sd-script1.sh
+++ sd-script1.sh
@@ -1,17 +1,28 @@
 #!/bin/bash
-# Database connection parameters
-# DB_HOST="selva-geneartdb.c7gozccxmuzm.us-east-1.rds.amazonaws.com"
-DB_HOST= ${DB_HOST:-default_value}
-DB_PORT="5432"
-DB_NAME="geneart"
-DB_USER="postgres"
-# DB_PASSWORD="$DB_PASSWORD"
-DB_PASSWORD="postgres"
-export PGPASSWORD="$DB_PASSWORD"
-# Check network connectivity
-echo "Testing connectivity to the database host..."
-echo "database host: '$DB_HOST'"
-psql -h $DB_HOST -p "$DB_PORT" -U "$DB_USER" -d "$DB_NAME" -c "SELECT 1;"
-# Connect to the database
-# pg_isready -h "$DB_HOST" -p "$DB_PORT" -U "$DB_USER" -d "$DB_NAME"
-# psql -h "$DB_HOST" -p "$DB_PORT" -U "$DB_USER" -d "$DB_NAME" -c "SELECT 1;"
+set -e
+
+# Ensure the environment variables are available
+DB_HOST="${DB_HOST}"
+DB_PORT="${DB_PORT}"
+DB_NAME="${DB_NAME}"
+DB_USER="${DB_USER}"
+PGPASSWORD="${DB_PASSWORD}"  # Use the environment variable for password
+
+SQL_FILE="./release1.sql"
+
+# Check if the database is ready
+pg_isready -h "$DB_HOST" -p "$DB_PORT" -U "$DB_USER" -d "$DB_NAME"
+if [ $? -eq 0 ]; then
+    echo "Database is ready. Executing SQL file..."
+    # Execute the SQL file
+    psql -h "$DB_HOST" -p "$DB_PORT" -U "$DB_USER" -d "$DB_NAME" -f "$SQL_FILE"
+    if [ $? -eq 0 ]; then
+        echo "SQL file executed successfully"
+    else
+        echo "Error executing SQL file"
+        exit 1
+    fi
+else
+    echo "Database is not ready or authentication failed. Transaction terminated."
+    exit 1
+fi
