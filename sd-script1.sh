e3e264f40b0e8c97f04a7942dab4f832f6537957
Gunasekaran <703625094@SanfordHealth.org>
2024-05-30 12:36:43 -0500

    sh script change for the pipeline1

--- sd-script1.sh
+++ sd-script1.sh
@@ -10,5 +10,20 @@ SQL_FILE="./release1.sql"
 echo  "Selva"
 # Check if the database is ready
 # pg_isready -h "$DB_HOST" -p "$DB_PORT" -U "$DB_USER" -d "$DB_NAME"
+# pg_isready -h "$DB_HOST" -p "$DB_PORT"
+# psql -q -h "$DB_HOST" -p "$DB_PORT" -U "$DB_USER" -d "$DB_NAME" -f "$SQL_FILE"
 pg_isready -h "$DB_HOST" -p "$DB_PORT"
-psql -q -h "$DB_HOST" -p "$DB_PORT" -U "$DB_USER" -d "$DB_NAME" -f "$SQL_FILE"
+if [ $? -eq 0 ]; then
+    echo "Database is ready. Executing SQL file..."
+    # Execute the SQL file
+    psql -q -h "$DB_HOST" -p "$DB_PORT" -U "$DB_USER" -d "$DB_NAME" -f "$SQL_FILE"
+    if [ $? -eq 0 ]; then
+        echo "SQL file executed successfully"
+    else
+        echo "Error executing SQL file"
+        exit 1
+    fi
+else
+    echo "Database is not ready or authentication failed. Transaction terminated."
+    exit 1
+fi
