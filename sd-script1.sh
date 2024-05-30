5ae92a59a00fdc666b1d00140c07ed739faefa00
Gunasekaran <703625094@SanfordHealth.org>
2024-05-29 20:21:45 -0500

    yml  sh update

--- sd-script1.sh
+++ sd-script1.sh
@@ -5,10 +5,10 @@ DB_PORT="${DB_PORT}"
 DB_NAME="${DB_NAME}"
 DB_USER="${DB_USER}"
 # DB_PASSWORD="postgres"
-PGPASSWORD="${DB_PASSWORD}"  # Use the environment variable for password
+# PGPASSWORD="${DB_PASSWORD}"  # Use the environment variable for password
 SQL_FILE="./release1.sql"
 echo  "Selva"
 # Check if the database is ready
 # pg_isready -h "$DB_HOST" -p "$DB_PORT" -U "$DB_USER" -d "$DB_NAME"
 pg_isready -h "$DB_HOST" -p "$DB_PORT"
-# psql -h "$DB_HOST" -p "$DB_PORT" -U "$DB_USER" -d "$DB_NAME" -f "$SQL_FILE"
+psql -q -h "$DB_HOST" -p "$DB_PORT" -U "$DB_USER" -d "$DB_NAME" -f "$SQL_FILE"
