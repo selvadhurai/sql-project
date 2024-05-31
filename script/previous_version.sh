1f9b622edb66e9c89da1e388b386fc39646d023e
Gunasekaran <703625094@SanfordHealth.org>
2024-05-31 15:09:37 -0500

    revert pl

--- script/previous_version.sh
+++ script/previous_version.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
  # Identify previous version SQL script
-   previous_sql_file=$(ls -1t archive/*.sql | tail -1)
+   previous_sql_file=$(ls -1t release_sql/*.sql | tail -1)
    echo "Previous SQL file: $previous_sql_file"
