488b46ef3b8e38cdee72e39266c93213ead6c4e8
Gunasekaran <703625094@SanfordHealth.org>
2024-05-31 09:35:23 -0500

    sh collecting log

--- script/sd-script1.sh
+++ script/sd-script1.sh
@@ -1,29 +1,36 @@
 #!/bin/bash
+
+LOG_FILE="tmp/sd-script1.log"
+
+#clear the log file if it exists
+> $LOG_FILE
+echo "Starting script..." | tee -a $LOG_FILE
+
 # Ensure the environment variables are available
 DB_HOST="${DB_HOST}"
 DB_PORT="${DB_PORT}"
 DB_NAME="${DB_NAME}"
 DB_USER="${DB_USER}"
-# DB_PASSWORD="postgres"
-# PGPASSWORD="${DB_PASSWORD}"  # Use the environment variable for password
 SQL_FILE="./release_sql/release1.sql"
-echo  "Selva"
+
 # Check if the database is ready
 # pg_isready -h "$DB_HOST" -p "$DB_PORT" -U "$DB_USER" -d "$DB_NAME"
 # pg_isready -h "$DB_HOST" -p "$DB_PORT"
 # psql -q -h "$DB_HOST" -p "$DB_PORT" -U "$DB_USER" -d "$DB_NAME" -f "$SQL_FILE"
-pg_isready -h "$DB_HOST" -p "$DB_PORT"
+pg_isready -h "$DB_HOST" -p "$DB_PORT" | tee -a $LOG_FILE
 if [ $? -eq 0 ]; then
     echo "Database is ready. Executing SQL file..."
     # Execute the SQL file
-    psql -q -h "$DB_HOST" -p "$DB_PORT" -U "$DB_USER" -d "$DB_NAME" -f "$SQL_FILE"
+    psql -q -h "$DB_HOST" -p "$DB_PORT" -U "$DB_USER" -d "$DB_NAME" -f "$SQL_FILE" | tee -a $LOG_FILE
     if [ $? -eq 0 ]; then
-        echo "SQL file executed successfully"
+        echo "SQL file executed successfully" | tee -a $LOG_FILE
     else
-        echo "Error executing SQL file"
+        echo "Error executing SQL file" | tee -a $LOG_FILE
         exit 1
     fi
 else
-    echo "Database is not ready or authentication failed. Transaction terminated."
+    echo "Database is not ready or authentication failed. Transaction terminated." | tee -a $LOG_FILE
     exit 1
 fi
+
+echo "Script compeleted successfully." | tee -a $LOG_FILE
