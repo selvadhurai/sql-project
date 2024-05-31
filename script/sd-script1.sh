523cf4d1a36d9ff157d703888e53617e304a7f43
Gunasekaran <703625094@SanfordHealth.org>
2024-05-31 09:13:00 -0500

    newupdate May31

--- script/sd-script1.sh
+++ script/sd-script1.sh
@@ -6,7 +6,7 @@ DB_NAME="${DB_NAME}"
 DB_USER="${DB_USER}"
 # DB_PASSWORD="postgres"
 # PGPASSWORD="${DB_PASSWORD}"  # Use the environment variable for password
-SQL_FILE="./release1.sql"
+SQL_FILE="./release_sql/release1.sql"
 echo  "Selva"
 # Check if the database is ready
 # pg_isready -h "$DB_HOST" -p "$DB_PORT" -U "$DB_USER" -d "$DB_NAME"
