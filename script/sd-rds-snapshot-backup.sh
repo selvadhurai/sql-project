0784bcc8573033bc7876d515cdf13ff4bc93f64d
Gunasekaran <703625094@SanfordHealth.org>
2024-06-21 11:02:44 -0500

    snapshot sh update

--- script/sd-rds-snapshot-backup.sh
+++ script/sd-rds-snapshot-backup.sh
@@ -3,6 +3,7 @@
 LOG_FILE="/tmp/sd-script1.log"
 SNAPSHOT_NAME="selva-geneartdb-snapshot-$(date +%Y%m%d%H%M%S)"
 DB_INSTANCE_IDENTIFIER="selva-geneartdb"
+AWS_REGION="us-east-1"
 
 #clear the log file if it exists
 > $LOG_FILE
@@ -22,9 +23,37 @@ DB_PASSWORD="${DB_PASSWORD}"
 # psql -q -h "$DB_HOST" -p "$DB_PORT" -U "$DB_USER" -d "$DB_NAME" -f "$SQL_FILE"
 pg_isready -h "$DB_HOST" -p "$DB_PORT" | tee -a $LOG_FILE
 
-echo "Taking a snapshot of the database..." | tee -a $LOG_FILE
-aws rds create-db-snapshot --db-snapshot-identifier $SNAPSHOT_NAME --db-instance-identifier $DB_INSTANCE_IDENTIFIER | tee -a $LOG_FILE
-echo "Waiting for the snapshot to be available..." | tee -a $LOG_FILE
-aws rds wait db-snapshot-available --db-snapshot-identifier $SNAPSHOT_NAME | tee -a $LOG_FILE
+# timeout handling
+max_retries=5
+retry_count=0
+sleep_interval=60
 
-echo "Snapshot $SNAPSHOT_NAME is available." | tee -a $LOG_FILE
+echo "Taking a snapshot of the database..." | tee -a $LOG_FILE
+
+while [ $retry_count -lt $max_retries ]; do
+	aws rds create-db-snapshot --db-snapshot-identifier $SNAPSHOT_NAME --db-instance-identifier $DB_INSTANCE_IDENTIFIER | tee -a $LOG_FILE
+    if [ $? -eq 0]; then
+    	echo "Snapshot creation init successfully..." | tee -a $LOG_FILE
+    	break
+    else
+    	echo "Snapshot creation failed, Retry $sleep_interval seconds..." | tee -a $LOG_FILE
+    	sleeo $sleep_interval
+    	retry_count=$((retry_count + 1))
+    fi
+done
+
+if [ $retry_count -eq $max_retries ]; then
+	echo "Faild to init after $max_retries attempts. exiting.." | tee -a $LOG_FILE
+	exit 1
+fi
+echo "Waiting for the snapshot to be available.." | tee -a $LOG_FILE
+aws rds wait db-snapshot-available --db-snapshot-identifier $SNAPSHOT_NAME --region $AWS_REGION | tee -a $LOG_FILE
+
+if [ $? -eq 0 ]; then
+            echo "Snapshot $SNAPSHOT_NAME is available." | tee -a $LOG_FILE
+        else
+            echo "Failed to create snapshot. Exiting..." | tee -a $LOG_FILE
+            exit 1
+fi
+
+echo "Script completed successfully." | tee -a $LOG_FILE
