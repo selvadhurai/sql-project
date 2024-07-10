91401ef168ed2c0383c13d2152ca63345ce95be5
Gunasekaran <703625094@SanfordHealth.org>
2024-07-10 13:24:38 -0500

    modified yml

--- script/compare_exportsql.sh
+++ script/compare_exportsql.sh
@@ -23,20 +23,38 @@ pg_isready -h "$PREVIOUS_DB_HOST" -p "$PREVIOUS_DB_PORT" -U "$PREVIOUS_DB_USER" 
 # Export current and previous databases
 echo "Exporting current database..."
 export PGPASSWORD=$CURRENT_DB_PASSWORD
-pg_dump -s -h $CURRENT_DB_HOST -U $CURRENT_DB_USER -d $CURRENT_DB_NAME > $EXPORT_DIR/current_db_schema.sql
-pg_dump -a -h $CURRENT_DB_HOST -U $CURRENT_DB_USER -d $CURRENT_DB_NAME > $EXPORT_DIR/current_db_data.sql
+if ! pg_dump -s -h $CURRENT_DB_HOST -U $CURRENT_DB_USER -d $CURRENT_DB_NAME > $EXPORT_DIR/current_db_schema.sql; then
+  echo "Failed to export current database schema."
+  exit 1
+fi
+if ! pg_dump -a -h $CURRENT_DB_HOST -U $CURRENT_DB_USER -d $CURRENT_DB_NAME > $EXPORT_DIR/current_db_data.sql; then
+  echo "Failed to export current database data."
+  exit 1
+fi
 
 echo "Exporting previous database..."
 export PGPASSWORD=$PREVIOUS_DB_PASSWORD
-pg_dump -s -h $PREVIOUS_DB_HOST -U $PREVIOUS_DB_USER -d $PREVIOUS_DB_NAME > $EXPORT_DIR/previous_db_schema.sql
-pg_dump -a -h $PREVIOUS_DB_HOST -U $PREVIOUS_DB_USER -d $PREVIOUS_DB_NAME > $EXPORT_DIR/previous_db_data.sql
+if ! pg_dump -s -h $PREVIOUS_DB_HOST -U $PREVIOUS_DB_USER -d $PREVIOUS_DB_NAME > $EXPORT_DIR/previous_db_schema.sql; then
+  echo "Failed to export previous database schema."
+  exit 1
+fi
+if ! pg_dump -a -h $PREVIOUS_DB_HOST -U $PREVIOUS_DB_USER -d $PREVIOUS_DB_NAME > $EXPORT_DIR/previous_db_data.sql; then
+  echo "Failed to export previous database data."
+  exit 1
+fi
+
 # Compare schemas using diff
 echo "Comparing schemas..."
-diff $EXPORT_DIR/previous_db_schema.sql $EXPORT_DIR/current_db_schema.sql > $EXPORT_DIR/$SCHEMA_DIFF_FILE
+if ! diff $EXPORT_DIR/previous_db_schema.sql $EXPORT_DIR/current_db_schema.sql > $EXPORT_DIR/$SCHEMA_DIFF_FILE; then
+  echo "Failed to compare schemas."
+  exit 1
+fi
 
 # Compare data using diff
 echo "Comparing data..."
-diff $EXPORT_DIR/previous_db_data.sql $EXPORT_DIR/current_db_data.sql > $EXPORT_DIR/$DATA_DIFF_FILE
-
+if ! diff $EXPORT_DIR/previous_db_data.sql $EXPORT_DIR/current_db_data.sql > $EXPORT_DIR/$DATA_DIFF_FILE; then
+  echo "Failed to compare data."
+  exit 1
+fi
 echo "Schema differences saved to $EXPORT_DIR/$SCHEMA_DIFF_FILE"
 echo "Data differences saved to $EXPORT_DIR/$DATA_DIFF_FILE"
